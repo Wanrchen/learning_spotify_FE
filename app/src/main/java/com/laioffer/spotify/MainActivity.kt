323eb4a08d533160bc6c5310678a27b6cef3455a
Yuanchen Wang <yw8063@nyu.edu>
2024-04-03 00:00:58 -0400

    init push

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,45 +1,189 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import coil.compose.AsyncImage
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
-class MainActivity : ComponentActivity() {
+// customized extend AppCompatActivity
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "lifecycle"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        //根据id来拿view
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        val navHostFragment =supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        navController.setGraph(R.navigation.nav_graph)
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
+        navView.setOnItemSelectedListener{
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, inclusive = false)
+            true
         }
+    }
+
+}
+
+@Composable
+private fun AlbumCover() {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = "Still Fantasy",
+                modifier = Modifier
+                    .padding(start = 2.dp, bottom = 4.dp)
+                    .align(Alignment.BottomStart),
+                color = Color.White
+            )
+        }
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray
+        )
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.body2
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                onNameChange(it)
+            },
+            label = { Text("Name") }
+        )
+    }
 }
+
+
+// composition
+
+@Composable
+fun Greeting(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(24.dp)
+            .background(Color.Yellow)
+    ) {
+        Text(text = "Hello")
+        Text(text = name)
+    }
+
+}
+
+@Composable
+fun ArtistCardBox() {
+    Box {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+fun ArtistCardColumn() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes before")
+        ArtistCardRow()
+    }
+}
+
+@Composable
+fun ArtistCardRow() {
+    Row {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardColumn() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardColumn()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardRow() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardRow()
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            Greeting("iOS")
         }
     }
 }
